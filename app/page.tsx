ec5b178deed6486eb765ac41903651c3fbaa3298
Harsh Rawat <harshrawat.dev@gmail.com>
2025-10-30 20:19:54 +0530

    added background image

--- app/page.tsx
+++ app/page.tsx
@@ -20,10 +20,11 @@ export default function Home() {
         </header>
 
         {/* Hero + Features Section (First Fold) */}
-        <section className='border-b border-dashed border-border'>
+        <section className='border-b border-dashed border-border relative'>
           {/* Hero */}
-          <div className='px-4 md:px-8 pt-43 pb-30 md:pt-43 md:pb-30'>
-            <div className='max-w-3xl text-center mx-auto'>
+          <div className='px-4 md:px-8 pt-45 pb-31 md:pt-45 md:pb-31 relative bg-[url("/black-bg.png")] dark:bg-[url("/white-bg.png")] bg-cover bg-center bg-no-repeat'>
+            {/* <div className='px-4 md:px-8 pt-45 pb-31 md:pt-45 md:pb-31 relative bg-cover bg-center bg-no-repeat'> */}
+            <div className='max-w-3xl text-center mx-auto relative z-10'>
               <h1 className='font-heading text-4xl md:text-5xl lg:text-6xl font-bold leading-tight mb-4 text-foreground mx-auto'>
                 Transform Chats into
                 <br></br>
