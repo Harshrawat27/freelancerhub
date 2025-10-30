70de67666d28d4e6698b2c20caa8c4cbbd300631
Harsh Rawat <harshrawat.dev@gmail.com>
2025-10-30 23:38:59 +0530

    hide one image

--- app/page.tsx
+++ app/page.tsx
@@ -32,13 +32,13 @@ export default function Home() {
               className='object-cover opacity-70'
               loading='lazy'
             />
-            <Image
+            {/* <Image
               src='https://cdn.prod.website-files.com/66b995a8da9c768e10e66aed/6903a78a16f2bfd19b19c5ff_background-01.webp'
               alt='Light mode background'
               fill
               className='object-cover opacity-70'
               loading='lazy'
-            />
+            /> */}
 
             <div className='max-w-3xl text-center mx-auto relative z-10'>
               <h1 className='font-heading text-4xl md:text-5xl lg:text-6xl font-bold leading-tight mb-4 text-foreground mx-auto'>
