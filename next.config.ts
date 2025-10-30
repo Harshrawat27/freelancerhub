2fb7a1a79e3330ccd6ec927d54e1febf556790e9
Harsh Rawat <harshrawat.dev@gmail.com>
2025-10-30 19:49:14 +0530

    Fix: Configure Next.js to bundle Prisma correctly

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', '@prisma/engines'],
+  },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*', './node_modules/@prisma/client/**/*'],
+  },
 };
 
 export default nextConfig;
